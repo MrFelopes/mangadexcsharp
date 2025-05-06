5d9e7996b9e0c8a3752e34061d38d9f303d98153
MrFelopes <feliperfragoso@gmail.com>
2025-05-06 17:37:13 -0300

    Refatoração do projeto para organização, separando as funções do MangaController em MangaResponse.cs e MangaService.cs.

--- mangadex/Controllers/MangaController.cs
+++ mangadex/Controllers/MangaController.cs
@@ -1,51 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
-using System.Web;
+using mangadex.Services;
+using mangadex.Models;
 
 
-namespace mangadex.Controllers
+namespace mangadex.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MangaController(MangaService service) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MangaController(HttpClient httpClient) : ControllerBase
+    [HttpGet("random")]
+    public async Task<IActionResult> GetRandomManga()
     {
-        [HttpGet("random")]
-        public async Task<IActionResult> GetRandomManga()
+        var mangas = await service.BuscarMangaAsync();
+        if (mangas == null || mangas.Count == 0)
+            return NotFound("Nenhum mangá encontrado");
+
+        var random = new Random();
+        var escolhido = mangas [random.Next(mangas.Count)];
+        return Ok(escolhido);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllMangas()
+    {
+        var mangas = await service.BuscarMangaAsync();
+
+        if (mangas == null || mangas.Count == 0)
+            return NotFound("Nenhum mangá encontrado.");
+
+        var resultado = mangas.Select(m => new
         {
-            const string baseUrl = "https://api.mangadex.org/manga";
-
-            var query = HttpUtility.ParseQueryString(string.Empty);
-            query["limit"] = "100";
-            query["availableTranslatedLanguage[]"] = "pt-br";
-
-            var fullUrl = $"{baseUrl}?{query}";
-            
-            try
-            {
-                httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("MangaDexAPI/1.0");
-                var response = await httpClient.GetAsync(fullUrl);
-                if (!response.IsSuccessStatusCode)
-                {
-                    return StatusCode((int)response.StatusCode, $"Erro ao acessar a API. {response.StatusCode}");
-                }
-
-                var responseBody = await response.Content.ReadAsStringAsync();
-
-                var doc = JsonDocument.Parse(responseBody);
-                var root = doc.RootElement;
-
-                if (!root.TryGetProperty("data", out var dataArray) || dataArray.GetArrayLength() <= 0)
-                    return NotFound("Nenhum mangá encontrado.");
-                var random = new Random();
-                var randomIndex = random.Next(dataArray.GetArrayLength());
-
-                var randomManga = dataArray[randomIndex];
-                return Ok(randomManga);
-            }
-            catch (HttpRequestException ex)
-            {
-                return StatusCode(500, $"Erro ao fazer a requisição: {ex.Message}");
-            }
-        }
+            id = m.Id,
+            titulo = m.Attributes.ObterTitulo(),
+            status = m.Attributes.Status
+        });
+        return Ok(resultado);
     }
 }
